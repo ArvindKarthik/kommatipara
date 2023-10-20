48ec1799791235675f09e57420cef070de1b1d91
Arvind Karthik <arvindkarthik242@gmail.com>
2023-10-20 21:33:39 +0200

    Add log rotating policy

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from logging.handlers import RotatingFileHandler
 from utils import filter_data, rename_columns
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
@@ -63,5 +64,22 @@ def main():
     # Stop the Spark session
     spark.stop()
 
+def setup_logger(log_file):
+    """
+    setup_logger function configures a Rotating file policy to save log messages.
+    Log file will rotate when it reaches a size of 1MB
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 if __name__ == "__main__":
+    log_file = "kommatipara.log"
+    setup_logger(log_file)
     main()
+
+
+    
